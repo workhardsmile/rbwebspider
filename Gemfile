19d9da39761fc4e9e441952c57c6756cad290662
Frank Wu <gang.wu@istuary.com>
2017-05-11 11:08:11 +0800

    add usage description

--- Gemfile
+++ Gemfile
@@ -4,5 +4,4 @@ source 'https://ruby.taobao.org'
 
 gem "addressable"
 gem "rest-client"
-gem 'selenium-webdriver', '~>2.49.0'
 #gem "nokogiri", "1.6.8"
--- face_spider.rb
+++ face_spider.rb
@@ -1,4 +1,11 @@
 # encoding: UTF-8
+
+# Usage:
+#1.  download images while get URLs, total 100 threads:    
+#        ruby face_spider.rb    
+#2.  split to n, firstly get URLs, then download images:
+#        ruby face_spider.rb  2 1 1, ruby face_spider.rb  2 2 1  # $1=2: split to 2 parts, $2=1,2: the 1rd or the 2nd part, $3=1: total 100*2 threads for get URLs  
+#        ruby face_spider.rb  2 1 2, ruby face_spider.rb  2 2 2  # $1=2: split to 2 parts, $2=1,2: the 1rd or the 2nd part, $3=2: total 100*2 threads for download images
 require 'json'
 require 'thread'
 require 'open-uri'
@@ -54,7 +61,7 @@ end
 
 $split = ARGV[0].nil? ? 1 : ARGV[0].to_i
 $n = ARGV[1].nil? ? 1 : ARGV[1].to_i
-$flag = ARGV[2].nil? ? 1 : ARGV[2].to_i
+$flag = ARGV[2].nil? ? 0 : ARGV[2].to_i
 puts "#################Strat Task: the #{$n} in #{$split} total: #{$split},#{$n},#{$flag}"
 $queue_names = Queue.new
 $queue_urls = Queue.new
